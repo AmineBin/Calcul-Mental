55c235d3efd59e05fdb67f1b4aaacc7dc236eda3
ThomasB2005 <bustamante.t2005@gmail.com>
2025-05-20 19:32:29 +0200

    avance vite fait

--- app/src/main/java/com/example/calcul_mental/CalculMentalActivity.java
+++ app/src/main/java/com/example/calcul_mental/CalculMentalActivity.java
@@ -1,6 +1,17 @@
 package com.example.calcul_mental;
 
+import java.util.Random;
+
+
+
 public class CalculMentalActivity {
+    private Integer PremierElem;
+    private Integer DeuxiemeElem;
 
-
+    protected void Valeur(){
+        Random random1 = new Random();
+        this.PremierElem=random1.nextInt(100);
+        Random random2 = new Random();
+        if()
+    }
 }
